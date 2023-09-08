8ce1e8566ae99c59a75b084a80e6a475c046aa78
martincmr1 <martincmr1@hotmail.com>
2023-09-07 23:23:18 -0300

    desafioLogin

--- sources/newApp.js
+++ sources/newApp.js
@@ -8,6 +8,8 @@ const router = require("./router");
 const connectMongo = require("./db");
 const Messages = require("./Dao/models/messages.model");
 const cookieParser = require("cookie-parser");
+const session = require('express-session')
+const MongoStore= require('connect-mongo')
 
 const app = express();
 const httpServer = app.listen(8080, () => {
@@ -23,6 +25,21 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", process.cwd() + "/views");
 app.set("view engine", "handlebars");
 app.use(cookieParser());
+//app.use(session({
+//  secret:'secretCoder',
+//  resave:true,
+//  saveUninitialized:true,
+//}))
+
+app.use(session({
+  store:MongoStore.create({
+    mongoUrl:"mongodb+srv://martincmr1:admin@cluster0.0spsbev.mongodb.net/?retryWrites=true&w=majority",
+    ttl:15,
+  }),
+  secret:"asdr",
+  resave:false,
+  saveUninitialized:false,
+}))
 
 connectMongo();
 
@@ -72,5 +89,61 @@ app.get("/get", (req, res) => {
 app.get("/del", (req, res) => {
   res.clearCookie("server").send("cookie eliminada");
 });
+
+
+app.get('/',(req,res)=> {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/submit',(req,res) => {
+  const {name,email} =req.body
+  res.cookie('user',email,{maxAge:10000}).send('cookie creada')
+})
+
+app.get("/getCookie", (req, res) => {
+ const userCookie = req.cookies.user
+  res.send("cookie recibida" + userCookie)
+  console.log(userCookie)
+});
+
+////////////////////////session//////////////////////////////////
+
+app.get('/session',(req,res)=>{
+  if(req.session.counter++){
+  res.send(`se ha visitado el sitio ${req.session.counter} veces`)
+}
+else{
+  req.session.counter = 1
+  res.send('BIENVENIDO!!')
+}
+})
+
+app.get('/logout', (req,res) =>{
+  req.session.destroy(err =>{
+    if(!err) res.send('logout ok!')
+    else res.send({status:'logout ERROR', body:err})
+
+  })
+})
+
+app.get('/login',(req,res)=> {
+  const { username,password} = req.query
+  if(username!== 'pepe' || password !== 'pepepass'){
+    return res.send('login failed')
+  }
+  req.session.user = username
+  req.session.admin= true
+  res.send('login success!')
+})
+
+app.get('/privado', (req,res)=>{
+  res.send('si estas viendo esto es porque te logueaste')
+})
+
+//////////////mongo storage////////////////////////
+
+
+
 
 module.exports = app;
