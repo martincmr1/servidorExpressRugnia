19332102af4885f28c1e81b4e0d9c116656edcd9
martincmr1 <martincmr1@hotmail.com>
2023-12-10 09:30:14 -0300

    routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { port } = require('./sources/config');
+/*const { port } = require('./sources/config');
 const realtimeServer= require('./sources/servers/realTimeServer')
 const app = require('./sources/servers/server.js')
 
@@ -14,8 +14,19 @@ const httpServer = app.listen(port, () => {
  realtimeServer(httpServer)
 
 
+*/
 
+const realtimeServer = require('./sources/servers/realTimeServer');
+const app = require('./sources/servers/server.js');
 
+// Utiliza process.env.PORT como el puerto de escucha para Vercel
+const port = process.env.PORT || 3000; // El 3000 es un puerto de respaldo si no se proporciona PORT
+
+const httpServer = app.listen(port, () => {
+  console.log(`Server running at port:${port}`);
+});
+
+realtimeServer(httpServer);
 
 
 
